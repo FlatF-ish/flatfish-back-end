68ff601be0456743cd4424ec084e3dc7c393fa4b
Joshua <joshua.richardson@myunidays.com>
2019-11-19 12:56:22 +0000

    Getting ready to update how routes work

--- facebook/message_paths/pathSetup.js
+++ facebook/message_paths/pathSetup.js
@@ -9,14 +9,28 @@ dbManager.register((client) => {
   pathDb = db.collection("pathData");
 });
 
+// var items = [
+//     {pathId: "help", type: "string", title: "How can I help", items: [{title: "Make a reservation", payload: "r"}, {title: "Something has run out, raise the alarm", payload: "out"}]}, 
+//     {pathId: "r", type: "button", title: "What would you like to reserve", items: [{title: "Reserve oven", payload: "ro"}, {title: "Reserve washing machine", payload: "rw"}]},
+//     {pathId: "ro", type: "integer", title: "Oven Reserved, would you like help with anything else?", items: [{title: "Yes", payload: "help"}, {title: "No", payload: "exit"}]}, 
+//     {pathId: "rw", type: "integer", title: "Washing Machine Reserved, would you like help with anything else?", items: [{title: "Yes", payload: "help"}, {title: "No", payload: "exit"}]}, 
+//     {pathId: "out", type: "button", title: "What has run out?", items: [{title: "Toilet paper", payload: "tp"}, {title: "Kitchen Roll", payload: "kr"}]},
+//     {pathId: "tp", type: "button", title: "Red alert for Toilet Paper sent, would you like help with anything else?", items: [{title: "Yes", payload: "help"}, {title: "No", payload: "exit"}]}, 
+//     {pathId: "kr", type: "button", title: "Red alert for Kitchen Roll sent, would you like help with anything else?", items: [{title: "Yes", payload: "help"}, {title: "No", payload: "exit"}]}, 
+  
+//     //{pathId: "", question: "", items: {title: "", payload: ""}}
+// ]
+
+
+
 var items = [
-    {pathId: "help", type: "string", title: "How can I help", items: [{title: "Make a reservation", payload: "r"}, {title: "Something has run out, raise the alarm", payload: "out"}]}, 
-    {pathId: "r", type: "button", title: "What would you like to reserve", items: [{title: "Reserve oven", payload: "ro"}, {title: "Reserve washing machine", payload: "rw"}]},
-    {pathId: "ro", type: "integer", title: "Oven Reserved, would you like help with anything else?", items: [{title: "Yes", payload: "help"}, {title: "No", payload: "exit"}]}, 
-    {pathId: "rw", type: "integer", title: "Washing Machine Reserved, would you like help with anything else?", items: [{title: "Yes", payload: "help"}, {title: "No", payload: "exit"}]}, 
-    {pathId: "out", type: "button", title: "What has run out?", items: [{title: "Toilet paper", payload: "tp"}, {title: "Kitchen Roll", payload: "kr"}]},
-    {pathId: "tp", type: "button", title: "Red alert for Toilet Paper sent, would you like help with anything else?", items: [{title: "Yes", payload: "help"}, {title: "No", payload: "exit"}]}, 
-    {pathId: "kr", type: "button", title: "Red alert for Kitchen Roll sent, would you like help with anything else?", items: [{title: "Yes", payload: "help"}, {title: "No", payload: "exit"}]}, 
+    {pathId: "help", title: "How can I help", items: [{title: "Make a reservation", payload: "r"}, {title: "Something has run out, raise the alarm", payload: "out"}], status: "generic", path: "", requiresMeta: "false", mataType: "null"}, 
+    {pathId: "r", title: "What would you like to reserve", items: [{title: "Reserve oven", payload: "ro"}, {title: "Reserve washing machine", payload: "rw"}], status: "generic", path: "", requiresMeta: "false", mataType: "null"},
+    {pathId: "ro", title: "Oven Reserved, would you like help with anything else?", items: [{title: "Yes", payload: "help"}, {title: "No", payload: "exit"}], status: "action", path: "reserve/oven/", requiresMeta: "true", mataType: "int"}, 
+    {pathId: "rw", title: "Washing Machine Reserved, would you like help with anything else?", items: [{title: "Yes", payload: "help"}, {title: "No", payload: "exit"}], status: "action", path: "reserve/washing-machine/", requiresMeta: "true", mataType: "int"}, 
+    {pathId: "out", title: "What has run out?", items: [{title: "Toilet paper", payload: "tp"}, {title: "Kitchen Roll", payload: "kr"}], status: "generic", path: "", requiresMeta: "false", mataType: "null"},
+    {pathId: "tp", title: "Red alert for Toilet Paper sent, would you like help with anything else?", items: [{title: "Yes", payload: "help"}, {title: "No", payload: "exit"}], status: "action", path: "out/toilet-paper/", requiresMeta: "false", mataType: "null"}, 
+    {pathId: "kr", title: "Red alert for Kitchen Roll sent, would you like help with anything else?", items: [{title: "Yes", payload: "help"}, {title: "No", payload: "exit"}], status: "action", path: "out/kitchen-roll/", requiresMeta: "false", mataType: "null"}, 
   
     //{pathId: "", question: "", items: {title: "", payload: ""}}
 ]
