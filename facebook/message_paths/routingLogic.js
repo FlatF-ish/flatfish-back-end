b21a2592a918d30a8e35c83f5d073ea942921f9f
Joshua <joshua.richardson@myunidays.com>
2019-11-20 06:31:06 +0000

    Added an outline for how the new routing logic should work

---
+++ facebook/message_paths/routingLogic.js
@@ -0,0 +1,16 @@
+// Get previous route  from lookup table
+// Get new route from database based on current action
+// Check status
+// If status is generic do ...
+    // Set existing path to empty
+// If status is action do ...
+    // Check path
+    // Set existing path to new path
+    // Check requiresMeta (bool)
+// If status is meta do ...
+    // Find out the expected type for the metadata
+    // Ask user to fill in the meta data
+    // Strip the metadata of anything that is not quite right to leave the expected value
+    // Append the prrocessed metadata to the existing path
+
+    
