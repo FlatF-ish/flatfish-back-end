5829f0b6a0ddacc9f961d72d90fbc45d5bb4b5ac
Joshua <joshua.richardson@myunidays.com>
2019-11-20 00:12:03 +0000

    Added rota to-do list

---
+++ rotas/rota.js
@@ -0,0 +1,16 @@
+//TO-DO
+// Create a way for rotas to be written to the database
+    // Create an endpoint to update a rota
+    // Create an endpoint to access speecific information
+    // Make a way to update who's turn it is to do something
+    // Make a way to say that you have completed your job
+    // Create a way to send a notification to someone who's turn it will be the next day
+    // Remind someone on the day that it is their go to do something
+    // Create a weekly overview message of everything - who will do what
+    // Allow users to say that they have done it or ask to be reminded later
+    // Keep track of who's turn it is to buy toilet paper next
+    // Keep track of who's turn it is to buy kitchen paper next
+    // Keep track of the current week
+    // Allow 1 user to send a message to trigger notification for another user
+
+// More to add - but I'm a sleeepy boi
