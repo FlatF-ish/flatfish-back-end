d3d857b4b46678054865e7a4a80a80dd036eaf0d
Joshua <psyjr4@nottingham.ac.uk>
2019-11-21 19:18:07 +0000

    Added updated routing

--- facebook/message_paths/pathSetup.js
+++ facebook/message_paths/pathSetup.js
@@ -24,14 +24,15 @@ dbManager.register((client) => {
 
 
 var items = [
-    {pathId: "help", title: "How can I help", items: [{title: "Make a reservation", payload: "r"}, {title: "Something has run out, raise the alarm", payload: "out"}], status: "generic", path: "", requiresMeta: "false", mataType: "null"}, 
+    {pathId: "help", title: "How can I help", items: [{title: "Make a reservation", payload: "r"}, {title: "Something has run out, raise the alarm", payload: "out"},  {title: "Control lighting", payload: "cl"}], status: "generic", path: "", requiresMeta: "false", mataType: "null"}, 
     {pathId: "r", title: "What would you like to reserve", items: [{title: "Reserve oven", payload: "ro"}, {title: "Reserve washing machine", payload: "rw"}], status: "generic", path: "", requiresMeta: "false", mataType: "null"},
     {pathId: "ro", title: "Oven Reserved, would you like help with anything else?", items: [{title: "Yes", payload: "help"}, {title: "No", payload: "exit"}], status: "action", path: "reserve/oven/", requiresMeta: "true", mataType: "int"}, 
     {pathId: "rw", title: "Washing Machine Reserved, would you like help with anything else?", items: [{title: "Yes", payload: "help"}, {title: "No", payload: "exit"}], status: "action", path: "reserve/washing-machine/", requiresMeta: "true", mataType: "int"}, 
     {pathId: "out", title: "What has run out?", items: [{title: "Toilet paper", payload: "tp"}, {title: "Kitchen Roll", payload: "kr"}], status: "generic", path: "", requiresMeta: "false", mataType: "null"},
     {pathId: "tp", title: "Red alert for Toilet Paper sent, would you like help with anything else?", items: [{title: "Yes", payload: "help"}, {title: "No", payload: "exit"}], status: "action", path: "out/toilet-paper/", requiresMeta: "false", mataType: "null"}, 
     {pathId: "kr", title: "Red alert for Kitchen Roll sent, would you like help with anything else?", items: [{title: "Yes", payload: "help"}, {title: "No", payload: "exit"}], status: "action", path: "out/kitchen-roll/", requiresMeta: "false", mataType: "null"}, 
-  
+    
+    {pathId: "cl", title: "What setting would you like?", items: [{title: "Random", payload: "1"}, {title: "Random", payload: "1"}, {title: "Still", payload: "7"}], status: "action", path: "/lighting-control/", requiresMeta: "true", mataType: "int"}, 
     //{pathId: "", question: "", items: {title: "", payload: ""}}
 ]
 
