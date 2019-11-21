0845948ec836e843ec881d045c6b844f9d07a10f
Joshua <psyjr4@nottingham.ac.uk>
2019-11-21 19:17:53 +0000

    Added description for what lights should do

---
+++ lighting_control/lights.js
@@ -0,0 +1,21 @@
+// Welcome to lighting control
+// controlling Christmas lights is going to be an important part of this app
+// The shorcut should be cl (control lights)
+// When you ask to control lights, you will get multiple options
+// The options returned could be in most used order - only 3 can be shown on the normal cards for facebook - useful if the top ones were here
+// Could be good to use a different sort of prompt e.g. on facebook the scrolling carousel at the bottom
+// This is an example of an action that will have meta data
+// Action = /control-lighting/
+// Metadata = a number representing the setting on the lights
+// The metadata will be associated with the names of the functions on the lights
+// When the endpoint for the setting is hit it will trigger an arduino controller to automatically reset the IC and then click through the number of times representing the setting
+
+// Themes of work
+// (PRIORITY)
+// Routing
+// Facebook
+// Controlling the lights
+
+// (NEXT STEPS)
+// App control panel
+// Associating specific lights - maybe? - might need a double metadata
