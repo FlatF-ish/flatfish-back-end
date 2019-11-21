2efd42d19aeeef058b3d319ebcf9909565a7cf7d
Joshua <psyjr4@nottingham.ac.uk>
2019-11-21 19:59:07 +0000

    Started out on a mission to create routing logic

--- facebook/message_paths/routingLogic.js
+++ facebook/message_paths/routingLogic.js
@@ -14,3 +14,52 @@
     // Append the prrocessed metadata to the existing path
 
     
+const dbmanager = require('../DBManager.js');
+
+var db;
+var pathDb;
+
+var userMessageTable; // need to decide how to implement this - maybe a class?
+
+dbmanager.register((client) => {
+  db = client.db("facebookData")
+
+  pathDb = db.collection("pathData");
+});
+
+
+function setPathOnMessage() {
+    const route = await pathDb.findOne({ pathId: path.toLowerCase() });
+    
+    if (userMessageTable.requiresMeta){
+        if (userMessageTable.metaDataType === "int") {
+            console.log("parse message as int");
+            console.log("Ensure that only an int is remaining");
+            console.log("If there is any confusion then send another message asking for clarification");
+        }
+    }
+
+    switch(path.status)
+    {
+        case "action":
+            userMessageTable.path = route.path;
+            if(route.requiresMeta) {
+                // Send a message asking for metadata of type
+                console.log("You require" + route.mataType)
+                userMessageTable.metaTypeExpected = route.metaType;
+            } else {
+                console.log("End of action");
+                console.log("Hit endpoint from here");
+            }
+            break;
+        case "generic":
+            userMessageTable.path = null;
+            break;
+        default: 
+            userMessageTable.path = null;
+            break;
+    }
+}
+
+
+module.exports = {setPathOnMessage: setPathOnMessage}
