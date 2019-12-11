3dc8344cee38b1d1923f1e7950b13ec9a03e2bdd
Samuel Williams <williamssamuel1999@gmail.com>
2019-12-11 18:34:34 +0000

    Fixed and Linted pathSetup.js

--- src/util/pathSetup.js
+++ src/util/pathSetup.js
@@ -1,12 +1,12 @@
-const dbManager = global.include("./util/DBManager.js");
+const dbManager = global.include('./util/DBManager.js');
 
 var pathDb;
 var app = global.app;
 
 dbManager.register((client) => {
-	var db = client.db("facebookData");
+	var db = client.db('facebookData');
 
-	pathDb = db.collection("pathData");
+	pathDb = db.collection('pathData');
 });
 
 // var items = [
@@ -36,143 +36,159 @@ dbManager.register((client) => {
 // ];
 
 
-
 var items = [
 	{
-		actionIndex: "help",
-		path: "", 
-		botAliases: ["help", "?", "h"],
-		type: "invoke",
-		title: "How can I help",
+		actionIndex: 'help',
+		path: '',
+		botAliases: ['help', '?', 'h'],
+		type: 'invoke',
+		title: 'How can I help',
 		options: [
-			{ title: "Make a reservation", payload: "r" },
-			{ title: "Something has run out, raise the alarm", payload: "out" },
-			{ title: "Control lighting", payload: "cl" }
+			{ title: 'Make a reservation', payload: 'reserve' },
+			{ title: 'Something has run out, raise the alarm', payload: 'out' },
+			{ title: 'Control lighting', payload: 'lights' }
 		],
-		requireAccount: "false" 
+		requireAccount: 'false'
 	},
 
 	{
-		actionIndex: "r",
-		path: "", 
-		botAliases: ["reserve", "r"], 
-		type: "invoke", 
-		title: "What would you like to reserve",
+		actionIndex: 'reserve',
+		path: '',
+		botAliases: ['reserve', 'r'],
+		type: 'invoke',
+		title: 'What would you like to reserve',
 		options: [
-			{ title: "Reserve oven", payload: "ro" }, 
-			{ title: "Reserve washing machine", payload: "rw" }
-		], 
-		requireAccount: "true" 
+			{ title: 'Reserve oven', payload: 'reserve-oven' },
+			{ title: 'Reserve washing machine', payload: 'reserve-washing-machine' }
+		],
+		requireAccount: 'true'
 	},
 
 	{
-		actionIndex: "ro",
-		path: "reserve/oven/", 
-		botAliases: ["oven", "reserve-oven", "ro"], 
-		type: "action", 
-		options: [{title: "Yes", payload: "help"}], 
-		steps: [{title: "How long would you like to reserve the oven for?", key: "reserve-time". type="int", min="5", max="60"}], 
-		postInvoke: "anything-else", 
-		requireAccount: "true"
+		actionIndex: 'reserve-oven',
+		path: 'reserve/oven/',
+		botAliases: ['oven', 'reserve-oven', 'ro'],
+		type: 'action',
+		steps: [
+			{
+				title: 'How long would you like to reserve the oven for?',
+				key: 'reserve-time',
+				type: 'int',
+				min: 5,
+				max: 60
+			}
+		],
+		postInvoke: 'anything-else',
+		requireAccount: 'true'
 	},
 	{
-		actionIndex: "rw",
-		path: "reserve/washing-machine", 
-		botAliases: ["rw", "reserve-washing-machine", "washing-machine"], 
-		type: "action", 
-		options: [{ title: "Yes", payload: "help" }, { title: "No", payload: "exit" }], 
-		steps: [{title: "How long would you like to reserve the washing machine for?", key: "reserve-time". type="int", min="50", max="240"}], 
-		postInvoke: "anything-else", 
-		requireAccount: "true" 
+		actionIndex: 'reserve-washing-machine',
+		path: 'reserve/washing-machine',
+		botAliases: ['rw', 'reserve-washing-machine', 'washing-machine'],
+		type: 'action',
+		steps: [
+			{
+				title: 'How long would you like to reserve the washing machine for?',
+				key: 'reserve-time',
+				type: 'int',
+				min: 50,
+				max: 240
+			}
+		],
+		postInvoke: 'anything-else',
+		requireAccount: 'true'
 	},
 	{
-		actionIndex: "out",
-		path: "invoke", 
-		botAliases: ["out", "run-out"], 
-		type: "action", 
-		title: "What has run out?",
-		options: [{ title: "Toilet paper", payload: "tp" }, { title: "Kitchen Roll", payload: "kr" }], 
-		requireAccount: "true" 
+		actionIndex: 'out',
+		path: '',
+		botAliases: ['out', 'run-out'],
+		type: 'invoke',
+		title: 'What has run out?',
+		options: [
+			{ title: 'Toilet paper', payload: 'out-toilet-paper' },
+			{ title: 'Kitchen Roll', payload: 'out-kitchen-roll' }
+		],
+		requireAccount: 'true'
 	},
 	{
-		actionIndex: "tp",
-		path: "out/toilet-paper/", 
-		botAliases: ["tp", "tr", "toilet-paper", "toilet-roll"], 
-		type: "action", 
-		title: "Red alert for Toilet Paper sent",
-		// options: [{ title: "Yes", payload: "help" }, { title: "No", payload: "exit" }], 
-		steps: [{title: "", key: "". type="", min="", max="", trueText="", falseText="", validityFunction: ""}], 
-		postInvoke: "anything-else", 
-		requireAccount: "true" 
+		actionIndex: 'out-toilet-paper',
+		path: 'out/toilet-paper/',
+		botAliases: ['tp', 'tr', 'toilet-paper', 'toilet-roll'],
+		type: 'action',
+		postInvoke: 'anything-else',
+		requireAccount: 'true'
 	},
 	{
-		actionIndex: "kr",
-		path: "out/kitchen-roll/", 
-		botAliases: ["kp", "kr", "kitchen-paper", "kitchen-roll"], 
-		type: "action",
-		title: "Red alert for Kitchen Roll sent",
-		// options: [{ title: "Yes", payload: "help" }, { title: "No", payload: "exit" }], 
-		// steps: [{title: "", key: "". type="", min="", max="", trueText="", falseText="", validityFunction: ""}], 
-		postInvoke: "anything-else", 
-		requireAccount: "true" 
+		actionIndex: 'out-kitchen-roll',
+		path: 'out/kitchen-roll/',
+		botAliases: ['kp', 'kr', 'kitchen-paper', 'kitchen-roll'],
+		type: 'action',
+		postInvoke: 'anything-else',
+		requireAccount: 'true'
+	},
+
+	// Lights
+	{
+		actionIndex: 'lights',
+		path: '',
+		botAliases: ['control-lights', 'light', 'lights', 'cl'],
+		type: 'invoke',
+		title: 'What would you like to do to the lights?',
+		options: [
+			{ title: 'Set state', payload: 'lights-state' },
+			{ title: 'Set mode', payload: 'lights-mode' }
+		],
+		requireAccount: 'true'
 	},
 	{
-		actionIndex: "cl",
-		path: "lighting-control/", 
-		botAliases: ["control-lights, light, lights, cl"], 
-		type: "action",
-		// title: "What setting would you like?",
-		// options: [{title: "", payload: ""}], 
-		steps: [{title: "What setting would you like?", key: "light-function". type="int", min="0", max="7"}], 
-		postInvoke: "anything-else", 
-		requireAccount: "true" 
+		actionIndex: 'lights-state',
+		path: 'lighting-control/',
+		botAliases: ['set-lights', 'turn-lights', 'sl'],
+		type: 'action',
+		steps: [
+			{
+				title: 'What state would you like to set the lights to?',
+				type: 'boolean',
+				key: 'state',
+				trueText: 'On',
+				falseText: 'Of'
+			}
+		],
+		postInvoke: 'anything-else',
+		requireAccount: 'true'
 	},
 	{
-		actionIndex: "anything-else",
-		path: "", 
-		botAliases: ["anything-else"], 
-		type: "invoke",
-		title: "Do you need anything else?",
-		options: [{title: "Yes", payload: "help"}, {title: "No", payload: "exit"}], 
-		requireAccount: "false" 
+		actionIndex: 'lights-mode',
+		path: 'lighting-control/',
+		botAliases: ['lights-mode', 'lm'],
+		type: 'action',
+		steps: [
+			{
+				title: 'What setting would you like?',
+				key: 'light-function',
+				type: 'int',
+				min: 1,
+				max: 7
+			}
+		],
+		postInvoke: 'anything-else',
+		requireAccount: 'true'
 	},
-	// {
-	// 	actionIndex: "light-function",
-	// 	path: "", 
-	// 	botAliases: ["light-function"], 
-	// 	type: "",
-	// 	title: "Choose the light function",
-	// 	options: [{title: "", payload: ""}], 
-	// 	steps: [{title: "", key: "". type="", min="", max="", trueText="", falseText="", validityFunction: ""}], 
-	// 	postInvoke: "", 
-	// 	requireAccount: "" 
-	// },
-	// {
-	// 	actionIndex: "reserve-time"",
-	// 	path: "", 
-	// 	botAliases: [""], 
-	// 	type: "",
-	// 	title: "",
-	// 	options: [{title: "", payload: ""}], 
-	// 	steps: [{title: "", key: "". type="", min="", max="", trueText="", falseText="", validityFunction: ""}], 
-	// 	postInvoke: "", 
-	// 	requireAccount: "" 
-	// },
-]
 
 
-// {
-// 	actionIndex: "",
-// 	path: "", 
-// 	botAliases: [""], 
-// 	type: "", 
-// 	options: [{title: "", payload: ""}], 
-// 	steps: [{title: "", key: "". type="", min="", max="", trueText="", falseText="", validityFunction: ""}], 
-// 	postInvoke: "", 
-// 	requireAccount: "" 
-// }
-
-
+	{
+		actionIndex: 'anything-else',
+		path: '',
+		botAliases: ['anything-else'],
+		type: 'invoke',
+		title: 'Do you need anything else?',
+		options: [
+			{ title: 'Yes', payload: 'help' },
+			{ title: 'No', payload: 'exit' }
+		],
+		requireAccount: 'false'
+	},
+];
 
 
 function addPathsToDatabase() {
@@ -181,10 +197,10 @@ function addPathsToDatabase() {
 	);
 }
 
-app.get("/reset-paths", (req, res) => {
-	if (!dbManager.isConnected()) { res.status(400).send("Try again my g"); return; }
+app.get('/reset-paths', (req, res) => {
+	if (!dbManager.isConnected()) { res.status(400).send('Try again my g'); return; }
 	pathDb.deleteMany().then(() => {
 		addPathsToDatabase();
-		res.status(200).send("All done!");
+		res.status(200).send('All done!');
 	});
 });
