160eb45443ba400beb4cd80e1a3f50c45271026a
Samuel Williams <williamssamuel1999@gmail.com>
2019-12-14 02:46:19 +0000

    Added splitMessage and commented parseInput.js, started bot routing

--- src/bot/botHandler.js
+++ src/bot/botHandler.js
@@ -5,7 +5,7 @@
 // This part of the system will also be responsible dealing with the routes in the routing table
 // Only do things on end of action - either meta is false - action ends or meta is true and data is provided (couold be more complex and require multipl emeta in future) - this is a simplification
 
-const dbManager = global.include("./util/DBManager.js");
+const dbManager = global.include('./util/DBManager.js');
 
 var userDb;
 var db;
@@ -14,14 +14,14 @@ var botCallbacks = {};
 var botStates = {};
 
 dbManager.register((client) => {
-	db = client.db("houseData");
-	userDb = db.collection("users");
+	db = client.db('houseData');
+	userDb = db.collection('users');
 });
 
 function registerBot(name, sendMessage) {
-	if (typeof name !== "string" || name.length === 0) { throw new Error("Bot name must be a string."); }
-	if (typeof sendMessage !== "function") { throw new Error("Bot sendMessage must be a function."); }
-	if (botCallbacks[name]) { throw new Error("Bot \"" + name + "\" already exists."); }
+	if (typeof name !== 'string' || name.length === 0) { throw new Error('Bot name must be a string.'); }
+	if (typeof sendMessage !== 'function') { throw new Error('Bot sendMessage must be a function.'); }
+	if (botCallbacks[name]) { throw new Error('Bot "' + name + '" already exists.'); }
 	botCallbacks[name] = sendMessage;
 	botStates[name] = {};
 }
@@ -37,8 +37,14 @@ function sendAll(userId, message) {
 // BSID = Bot specific ID, like facebookId or discordId
 function processMessage(botName, user, BSID, message) {
 	const state = botStates[botName];
-	if (state === undefined) throw new Error("Invalid bot name \"" + botName + "\"");
+	if (state === undefined) throw new Error('Invalid bot name "' + botName + '"');
 
+	const clientState = state[BSID];
+	if (clientState === undefined) {
+		// New action
+	} else {
+		// Handle param
+	}
 	// Do all the database stuff here :o
 }
 
--- src/util/parseInput.js
+++ src/util/parseInput.js
@@ -3,8 +3,15 @@ const VF = global.include('./util/validationFunctions.js');
 // To keep these formatted nicely
 const booleanTrueValues =  ['true',  't', '1', 'on',  'yes', 'y', 'sure'];
 const booleanFalseValues = ['false', 'f', '0', 'off', 'no',  'n', 'nah' ];
+
+// Replaces a character at an index, thought this was as simple as str[i] = 'y' but noooo
+String.prototype.replaceAt = function(index, replacement) {
+	return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+};
 /* eslint-enable */
 
+// First check type and validate
+// Then check for validityFunc, call and parse output
 async function parseInput(step, value, user) {
 	const func = parseFunctions[step.type];
 	if (func === undefined) {
@@ -26,11 +33,52 @@ function isPositiveInteger(n) {
 	return n >>> 0 === parseFloat(n);
 }
 
+// Split messages like:
+// this is a test => [this, is, a, test]
+// this is a "real test" => [this, is, a, real test]
+// this is a "cool \" hecking test" => [this, is, a, cool " hecking test]
+function splitMessage(msg) {
+	let token = '';
+	let inQuotes = null;
+	const out = [];
+	for (let i = 0; i < msg.length; i++) {
+		const c = msg[i];
+		if (inQuotes) {
+			switch (c) {
+				case inQuotes:
+					if (i > 0 && msg[i - 1] === '\\') token = token.replaceAt(token.length - 1, c);
+					else inQuotes = null;
+					continue;
+			}
+		} else {
+			switch (c) {
+				case ' ':
+					out.push(token);
+					token = '';
+					continue;
+				case '"': case '\'':
+					if (i > 0 && msg[i - 1] === '\\') token = token.replaceAt(token.length - 1, c);
+					else inQuotes = c;
+					continue;
+			}
+		}
+		token += c;
+	}
+	if (token.length > 0) out.push(token);
+	return out;
+}
+
+// Return format = {value: val} or {error: error}
 var parseFunctions = {
+	// Text is raw, just return value
 	text: function(step, value) {
 		return { value: value };
 	},
+	// Same as text
 	raw: (...data) => parseFunctions.text(...data),
+	// Get min and max (or take defaults)
+	// Get string versions of them, printed ranges like (0, 180427349023) don't look very nice
+	// Parse, check not NaN and in range
 	float: function(step, value) {
 		const min = step.min || 0;
 		const max = step.max || Number.MAX_SAFE_INTEGER;
@@ -45,12 +93,14 @@ var parseFunctions = {
 			return { value: parsed };
 		}
 	},
+	// Call above and ensure value % 1 == 0
 	int: function(step, value) {
 		const ret = parseFunctions.float(step, value);
 		if (ret.error) return ret;
 		if (ret.value % 1 !== 0) return { error: '"' + ret.value + '" is not an integer' };
 		return ret;
 	},
+	// Check against list of true and false values above
 	boolean: function(step, value) {
 		const lowerValue = value.toLowerCase();
 		if (booleanTrueValues.indexOf(lowerValue) !== -1) {
@@ -61,10 +111,12 @@ var parseFunctions = {
 			return { error: '"' + value + '" could not be recognised as a boolean value, try true/false, y/n, on/off, etc.' };
 		}
 	},
+	// Allow for options input by typing their payload, or doing #index
 	options: function(step, value) {
 		if (!step.options) throw new Error('No options in step');
 
 		if (value[0] === '#' && isPositiveInteger(value.substring(1))) {
+			// Selecting by doing #1 to #length
 			const idx = +value.substring(1);
 			if (idx >= 1 && idx <= step.options.length) {
 				return { value: step.options[idx - 1].payload };
@@ -72,6 +124,7 @@ var parseFunctions = {
 				return { error: 'Index "' + idx + '" not in range (1,' + step.options.length + ')' };
 			}
 		} else {
+			// Selecting by typing payload (case insensitive)
 			const options = [];
 			for (const option of step.options) {
 				options.push(option.payload.toLowerCase());
@@ -86,4 +139,4 @@ var parseFunctions = {
 	}
 };
 
-module.exports = { parse: parseInput };
+module.exports = { parse: parseInput, split: splitMessage };
